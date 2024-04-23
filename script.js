51aa64be30ca24174719488aea990589de3087b7
BishalAd <adbishal569@gmail.com>
2024-04-23 20:26:44 +0545

    First Version of Website

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    var typed = new Typed(".typing", {
+        strings: ["Help You Adopt Dogs", "Provide Dog Accessories", "Assist with Dog Training", "Ensure Check-ups and Care", "Offer Dog Boarding Services", "Connect You with Vets"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+});
