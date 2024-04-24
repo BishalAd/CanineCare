1b7a8cf2e245aff52d83017ea9cafe99b5ba1443
BishalAd <adbishal569@gmail.com>
2024-04-24 19:39:05 +0545

    Media Query update

--- script.js
+++ script.js
@@ -6,3 +6,13 @@ $(document).ready(function () {
         loop: true
     });
 });
+
+function showSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'flex'
+}
+
+function hideSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'none'
+}
